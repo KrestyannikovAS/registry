3cca950d7cfeca531e2908b614544d7bbf81aaa0
KrestyannikovAS <62802551+KrestyannikovAS@users.noreply.github.com>
2021-08-12 22:04:44 +0700

    auth

--- registry/registry/main/urls.py
+++ registry/registry/main/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('create', views.create, name='create'),
-    path('search/', views.search, name='search'),
 ]
--- registry/registry/main/views.py
+++ registry/registry/main/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render, redirect
-
 from .forms import DocumentForm
 from .models import Documents
 from django.db.models import Q
+import datetime
 
 
 def index(request):
+    now = datetime.date.today()
+    begin_time = now - datetime.timedelta(days=3650)
     order_by = request.GET.get('order_by', 'approval_date')
-    documents = Documents.objects.all().order_by(order_by)
-    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'documents': documents})
-
-
-def search(request):
     query = request.GET.get('q')
     if query is None:
         query = ''
-    documents = Documents.objects.filter(Q(title__icontains=query) | Q(doc_author__icontains=query) |
-                                         Q(approval_date__icontains=query) | Q(note__icontains=query))
-    return render(request, 'main/search_results.html', {'title': 'Результат поиска', 'documents': documents})
+    date_start = request.GET.get('date_start')
+    if date_start is None:
+        date_start = begin_time
+    date_end = request.GET.get('date_end')
+    if date_end is None:
+        date_end = now
+    if date_start <= date_end:
+        documents = Documents.objects.filter(Q(title__icontains=query) |
+                                             Q(doc_author__icontains=query) |
+                                             Q(approval_date__icontains=query) |
+                                             Q(note__icontains=query)).filter(Q(approval_date__gte=date_start) &
+                                                                              Q(approval_date__lte=date_end)).\
+            order_by(order_by)
+        return render(request, 'main/index.html', {'title': 'Результат поиска', 'documents': documents,
+                                                            'q': query})
+    else:
+        error = 'Форма была неверной'
+        return render(request, 'main/index.html', {'title': 'Результат поиска', 'error': error})
 
 
 def about(request):

ad1c2405bec204a5efac9f80cded7fad3eaa2889
KrestyannikovAS <62802551+KrestyannikovAS@users.noreply.github.com>
2021-10-25 21:48:54 +0700

    add list authors

--- registry/registry/main/urls.py
+++ registry/registry/main/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('create', views.create, name='create'),
+    path('change/<int:id>/', views.change, name='change'),
+    path('delete/<int:id>/', views.delete, name='delete'),
 ]
--- registry/registry/main/views.py
+++ registry/registry/main/views.py
@@ -2,19 +2,26 @@ from django.shortcuts import render, redirect
 from .forms import DocumentForm
 from .models import Documents
 from django.db.models import Q
+from django.http import HttpResponseRedirect
+from django.http import HttpResponseNotFound
 import datetime
 
 
 def index(request):
     now = datetime.date.today()
-    begin_time = now - datetime.timedelta(days=3650)
+    warn_time = now - datetime.timedelta(days=1065)
+    err_time = now - datetime.timedelta(days=1095)
     order_by = request.GET.get('order_by', 'approval_date')
     query = request.GET.get('q')
+    person = request.GET.get('person')
+    authors = Documents.objects.values_list('doc_author', flat=True).distinct('doc_author')
     if query is None:
         query = ''
+    if person is None:
+        person = ''
     date_start = request.GET.get('date_start')
     if date_start is None:
-        date_start = begin_time
+        date_start = now - datetime.timedelta(days=3650)
     date_end = request.GET.get('date_end')
     if date_end is None:
         date_end = now
@@ -23,13 +30,14 @@ def index(request):
                                              Q(doc_author__icontains=query) |
                                              Q(approval_date__icontains=query) |
                                              Q(note__icontains=query)).filter(Q(approval_date__gte=date_start) &
-                                                                              Q(approval_date__lte=date_end)).\
+                                                                              Q(approval_date__lte=date_end) &
+                                                                              Q(doc_author__icontains=person)).\
             order_by(order_by)
-        return render(request, 'main/index.html', {'title': 'Результат поиска', 'documents': documents,
-                                                            'q': query})
+        return render(request, 'main/index.html', {'documents': documents, 'warn': warn_time,
+                                                   'err': err_time, 'authors': authors})
     else:
-        error = 'Форма была неверной'
-        return render(request, 'main/index.html', {'title': 'Результат поиска', 'error': error})
+        error = 'Даты утверждения в форме поиска указан не верно!'
+        return render(request, 'main/index.html', {'error': error})
 
 
 def about(request):
@@ -52,3 +60,32 @@ def create(request):
         'error': error
     }
     return render(request, 'main/create.html', context)
+
+
+# изменение данных в бд
+def change(request, id):
+    try:
+        document = Documents.objects.get(id=id)
+
+        if request.method == "POST":
+            document.title = request.POST.get("title")
+            document.doc_author = request.POST.get("doc_author")
+            document.approval_date = request.POST.get("approval_date")
+            document.doc_url = request.POST.get("doc_url")
+            document.note = request.POST.get("note")
+            document.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "main/change.html", {"document": document})
+    except Documents.DoesNotExist:
+        return HttpResponseNotFound("<h2>Документ для изменения не найден</h2>")
+
+
+# удаление данных из бд
+def delete(id):
+    try:
+        document = Documents.objects.get(id=id)
+        document.delete()
+        return HttpResponseRedirect("/")
+    except Documents.DoesNotExist:
+        return HttpResponseNotFound("<h2>Документ для удаления не найден</h2>")

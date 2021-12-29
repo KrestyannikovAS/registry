f999783d6fe7d3856048693368673bcb50b3f455
KrestyannikovAS <62802551+KrestyannikovAS@users.noreply.github.com>
2021-12-29 15:55:23 +0700

    Add columns

--- registry/registry/main/forms.py
+++ registry/registry/main/forms.py
@@ -5,7 +5,8 @@ from .models import Documents
 class DocumentForm(ModelForm):
     class Meta:
         model = Documents
-        fields = ["title", "doc_author", "approval_date", "doc_url", "note"]
+        fields = ["title", "doc_author", "doc_author_department", "approval_date", "revision_date", "doc_source",
+                  "doc_url", "note"]
         widgets = {
             "title": TextInput(attrs={
                 'class': 'form-control',
@@ -15,9 +16,21 @@ class DocumentForm(ModelForm):
                 'class': 'form-control',
                 'placeholder': 'Введите ФИО автора'
             }),
+            "doc_author_department": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите отдел (по умолчанию "СПАК")'
+            }),
             "approval_date": TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Введите дату утверждения'
+            }),
+            "revision_date": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите дату пересмотра'
+            }),
+            "doc_source": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите источники (по умолчанию "АСДОУ")'
             }),
             "doc_url": TextInput(attrs={
                 'class': 'form-control',
@@ -25,6 +38,6 @@ class DocumentForm(ModelForm):
             }),
             "note": TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Введите описание'
+                'placeholder': 'Введите комментарий (по умолчанию "НЕТ")'
             }),
         }
--- registry/registry/main/models.py
+++ registry/registry/main/models.py
@@ -5,11 +5,14 @@ from datetime import date
 class Documents(models.Model):
     DoesNotExist = None
     objects = None
-    title = models.CharField('Название документа', max_length=250)
+    title = models.CharField('Название документа', max_length=512)
     doc_author = models.CharField('Автор', max_length=50)
+    doc_author_department = models.CharField('Отдел', max_length=50, default="СПАК")
     approval_date = models.DateField('Дата утверждения', default=date.today)
+    revision_date = models.DateField('Дата утверждения', default=date.today)
+    doc_source = models.CharField('Источник', max_length=50, default="АСДОУ")
     doc_url = models.CharField('Ссылка на документ', max_length=250)
-    note = models.CharField('Описание', max_length=250)
+    note = models.CharField('Комментарий', max_length=250, default="НЕТ")
 
     def __str__(self):
         return self.title
